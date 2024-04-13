b6e724fa34892bc7b3518f543f637865468dd317
Devansh Khare <devanshkhare0@gmail.com>
2024-04-13 18:07:46 +0530

    main signout component

--- components/SignedOut.tsx
+++ components/SignedOut.tsx
@@ -12,12 +12,12 @@ const SignedOut = () => {
                 </div>
             </div>
             <div className='text-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col justify-center items-center gap-5'>
-                <h1 className='text-[3rem] font-[900]'>Unlimited movies, TV shows and more</h1>
+                <h1 className='text-[3rem] font-[900] w-max'>Unlimited movies, TV shows and more</h1>
                 <p className='text-[1.5rem] font-[400]'>Watch anywhere. Cancel anytime.</p>
                 <p className='text-[1.25rem] font-[400]'>Ready to watch? Enter your email to create or restart your membership</p>
                 <div className='flex flex-row gap-2 items-center'>
-                    <input type="text" className='h-[3rem] w-[20rem] bg-gray-600 bg-opacity-30 border-gray-500 border-[1px] rounded-[0.25rem] pl-4' placeholder='Email address'/>
-                    <a className='bg-[#E50914] p-[0.7rem] w-[10rem] rounded-[0.25rem] font-bold text-[1.1rem] hover:cursor-pointer'>Get Started &nbsp;&gt;</a>
+                    <input type="text" className='h-[3.5rem] w-[25rem] bg-gray-600 bg-opacity-30 border-gray-500 border-[1px] rounded-[0.25rem] pl-4' placeholder='Email address'/>
+                    <a className='bg-[#E50914] p-[0.8rem] rounded-[0.25rem] font-bold text-[1.3rem] hover:cursor-pointer tracking-widest'>Get Started &nbsp;&gt;</a>
                 </div>
             </div>
         </div>
