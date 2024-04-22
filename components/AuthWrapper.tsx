4aac379d435bdedcbe21c407b418644f949f91fa
Devansh Khare <devanshkhare0@gmail.com>
2024-04-22 19:20:24 +0530

    formatting

--- components/AuthWrapper.tsx
+++ components/AuthWrapper.tsx
@@ -1,23 +1,21 @@
-"use client"
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const AuthWrapper = ({ children }: { children: any }) => {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
   const [oldStatus, setOldStatus] = useState("");
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     if (oldStatus == "authenticated" && status == "unauthenticated") {
-      router.push("/")
+      router.push("/");
     }
-    setOldStatus(status)
-  }, [status])
+    setOldStatus(status);
+  }, [status]);
 
-  return (
-    <>{children}</>
-  )
-}
+  return <>{children}</>;
+};
 
-export default AuthWrapper
+export default AuthWrapper;
