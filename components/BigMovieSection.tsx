aad0c2064b0d0b7b9e6536b72058b234d4de9e6b
Devansh Khare <devanshkhare0@gmail.com>
2024-04-26 00:13:41 +0530

    removed clg

--- components/BigMovieSection.tsx
+++ components/BigMovieSection.tsx
@@ -5,7 +5,6 @@ const BigMovieSection = async() => {
   const movies = await getMovies();
   if(!movies?.length) return <div>Loading...</div>
   const randomMovie = movies[Math.floor(Math.random()*movies?.length)]
-  console.log(randomMovie)
   return (
     <div className='absolute w-full h-[56.5vh] top-0 text-white'>
       <video src={randomMovie?.videoUrl} autoPlay muted className='relative h-[100%] w-screen object-cover brightness-[60%]' loop></video>
