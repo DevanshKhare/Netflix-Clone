48950e02f00cc1f2c2efdb19bee755dc59ea7ca4
Devansh Khare <devanshkhare0@gmail.com>
2024-04-27 18:24:14 +0530

    created more info button

--- components/BigMovieSection.tsx
+++ components/BigMovieSection.tsx
@@ -1,5 +1,6 @@
 import { getMovies } from '@/lib/actions/movie.actions';
 import React from 'react'
+import { IoIosInformationCircleOutline } from "react-icons/io";
 
 const BigMovieSection = async() => {
   const movies = await getMovies();
@@ -11,6 +12,10 @@ const BigMovieSection = async() => {
       <div className='absolute top-[40%] left-[5rem] max-w-[40%]'>
         <h1 className='text-[2rem]'>{randomMovie?.title}</h1>
         <p className='text-slate-300'>{randomMovie?.description}</p>
+        <button className='mt-5 text-white p-2 rounded-md bg-white bg-opacity-30 hover:bg-opacity-20 flex items-center'>
+          <IoIosInformationCircleOutline className='mr-1' fontSize={20}/>
+          More Info
+        </button>
       </div>
     </div>
   )
