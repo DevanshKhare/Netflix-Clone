e9bde38c8cf56316b6736213f3390a6565c864c5
Devansh Khare <devanshkhare0@gmail.com>
2024-04-27 20:42:49 +0530

    moved movie fetching to unified location

--- components/BigMovieSection.tsx
+++ components/BigMovieSection.tsx
@@ -1,16 +1,17 @@
 import { getMovies } from '@/lib/actions/movie.actions';
+import { movieInterface } from '@/lib/database/models/movie.model';
 import React from 'react'
 import { IoIosInformationCircleOutline } from "react-icons/io";
 
-const BigMovieSection = async() => {
-  const movies = await getMovies();
+const BigMovieSection = async({movies}:{movies: movieInterface[]}) => {
+  // const movies = await getMovies();
   if(!movies?.length) return <div>Loading...</div>
   const randomMovie = movies[Math.floor(Math.random()*movies?.length)]
   return (
-    <div className='absolute w-full h-[56.5vh] top-0 text-white'>
-      <video src={randomMovie?.videoUrl} autoPlay muted className='relative h-[100%] w-screen object-cover brightness-[60%]' loop></video>
-      <div className='absolute top-[40%] left-[5rem] max-w-[40%]'>
-        <h1 className='text-[2rem]'>{randomMovie?.title}</h1>
+    <div className='relative w-full h-[56.5vh] top-0 text-white'>
+      <video src={randomMovie?.videoUrl} muted className='relative h-[100%] w-screen object-cover brightness-[60%]' loop></video>
+      <div className='absolute top-[30%] left-[5rem] max-w-[40%]'>
+        <h1 className='text-[2rem] mb-[2rem]'>{randomMovie?.title}</h1>
         <p className='text-slate-300'>{randomMovie?.description}</p>
         <button className='mt-5 text-white p-2 rounded-md bg-white bg-opacity-30 hover:bg-opacity-20 flex items-center'>
           <IoIosInformationCircleOutline className='mr-1' fontSize={20}/>
