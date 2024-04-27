60cd03d4199de6fd107169c8deb5fad1caa48ac8
Devansh Khare <devanshkhare0@gmail.com>
2024-04-27 20:43:17 +0530

    created trending section

---
+++ components/Trending.tsx
@@ -0,0 +1,27 @@
+import { movieInterface } from "@/lib/database/models/movie.model";
+import Image from "next/image";
+import React from "react";
+
+const Trending = ({ movies }: { movies: movieInterface[] }) => {
+  return (
+    <div className="text-white relative w-[95%] m-auto mt-5 text-[1.5rem]">
+      <h1>Trending Now</h1>
+      <div className="h-auto flex flex-wrap mt-2 cursor-pointer">
+        {movies.map((movie) => (
+          <div className="m-2 rounded-sm">
+            <Image
+              src={movie?.thumbnailUrl}
+              alt="thumbnail"
+              width={0}
+              height={0}
+              className="h-[8rem] w-[16rem] rounded-sm"
+              unoptimized
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
