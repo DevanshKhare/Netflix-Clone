dff4cce94a9d3100a64904c5ccbb5b8b2f31a3a7
Devansh Khare <devanshkhare0@gmail.com>
2024-04-27 20:43:06 +0530

    added logic for movie fetching at profile page

--- app/profiles/[profile]/page.tsx
+++ app/profiles/[profile]/page.tsx
@@ -1,12 +1,17 @@
 import BigMovieSection from '@/components/BigMovieSection'
 import Navbar from '@/components/Navbar'
+import Trending from '@/components/Trending'
+import { getMovies } from '@/lib/actions/movie.actions'
+import { movieInterface } from '@/lib/database/models/movie.model'
 import React from 'react'
 
-const page = ({params}: {params: {profile: string}}) => {
+const page = async({params}: {params: {profile: string}}) => {
+  const movies: movieInterface[] = await getMovies() || [];
   return (
     <div className='bg-[#0f1014] h-screen'>
       <Navbar/>
-      <BigMovieSection/>
+      <BigMovieSection movies={movies}/>
+      <Trending movies={movies}/>
     </div>
   )
 }
