9a8fdd529dc69a3854a7c383a4198d1aa2784322
Devansh Khare <devanshkhare0@gmail.com>
2024-04-27 20:44:20 +0530

    added autoplay

--- components/BigMovieSection.tsx
+++ components/BigMovieSection.tsx
@@ -9,7 +9,7 @@ const BigMovieSection = async({movies}:{movies: movieInterface[]}) => {
   const randomMovie = movies[Math.floor(Math.random()*movies?.length)]
   return (
     <div className='relative w-full h-[56.5vh] top-0 text-white'>
-      <video src={randomMovie?.videoUrl} muted className='relative h-[100%] w-screen object-cover brightness-[60%]' loop></video>
+      <video src={randomMovie?.videoUrl} autoPlay muted className='relative h-[100%] w-screen object-cover brightness-[60%]' loop></video>
       <div className='absolute top-[30%] left-[5rem] max-w-[40%]'>
         <h1 className='text-[2rem] mb-[2rem]'>{randomMovie?.title}</h1>
         <p className='text-slate-300'>{randomMovie?.description}</p>
