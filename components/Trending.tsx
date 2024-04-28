adde371a83dbe3a9b81f664d4b74bdcd7defa9da
Devansh Khare <devanshkhare0@gmail.com>
2024-04-28 23:51:46 +0530

    unused log removed

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { FaCirclePlay } from "react-icons/fa6";
 
 const Trending = ({ movies }: { movies: movieInterface[] }) => {
-  console.log(movies)
   return (
     <div className="text-white relative w-[95%] m-auto mt-5">
       <h1 className=" text-[1.5rem]">Trending Now</h1>
