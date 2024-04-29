c50f417c7553eef6f28d586b98027cb765517a45
Devansh Khare <devanshkhare0@gmail.com>
2024-04-29 18:13:54 +0530

    add to favourites intitial commit

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,17 +1,22 @@
-import { addOrRemoveFavourite } from "@/lib/actions/user.actions";
 import { movieInterface } from "@/lib/database/models/movie.model";
 import React from "react";
 
 import MovieCard from "./MovieCard";
+import { getUserByEmail } from "@/lib/actions/user.actions";
+import { getServerSession } from "next-auth";
 
-const Trending = ({ movies }: { movies: movieInterface[] }) => {
-
+const Trending = async({ movies }: { movies: movieInterface[] }) => {
+  const session= await getServerSession()
+  let user: any;
+  if(session?.user?.email){
+    user = await getUserByEmail(session?.user?.email)
+  }
   return (
     <div className="text-white relative w-[95%] m-auto mt-5">
       <h1 className=" text-[1.5rem]">Trending Now</h1>
       <div className="h-auto flex flex-wrap mt-2">
         {movies.map((movie) => (
-          <MovieCard movie={movie}/>
+          <MovieCard movie={movie} favourites={user?.favourites} user={user?.email}/>
         ))}
       </div>
     </div>
