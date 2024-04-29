027750d69404b233f2d7ec4bb1b4df657019c1cc
Devansh Khare <devanshkhare0@gmail.com>
2024-04-29 16:58:51 +0530

    seprated out movie component

---
+++ components/MovieCard.tsx
@@ -0,0 +1,39 @@
+import { movieInterface } from "@/lib/database/models/movie.model";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import React from "react";
+import { FaCirclePlay } from "react-icons/fa6";
+import { IoAddCircleOutline } from "react-icons/io5";
+
+const MovieCard = ({ movie }: { movie: movieInterface }) => {
+  const { data: session } = useSession();
+  const handleFavourite = async (event: any) => {
+    console.log(event.target, movie);
+    // await addOrRemoveFavourite(session?.user?.email)
+  };
+  return (
+    <div className="m-2 rounded-sm cursor-pointer hover:scale-110 hover:translate-y-[-2vw] trending">
+      <Image
+        src={movie?.thumbnailUrl}
+        alt="thumbnail"
+        width={0}
+        height={0}
+        className="h-[8rem] w-[16rem] rounded-sm"
+        unoptimized
+      />
+      <div className="w-full h-full opacity-0 trendingDesc mt-5 text-[0.8rem] pl-2">
+        <FaCirclePlay fontSize={30} className="mb-2 inline-block" />
+        <IoAddCircleOutline
+          fontSize={30}
+          className="ml-2 mb-2 inline-block"
+          onClick={handleFavourite}
+        />
+        <h1 className="text-[1.2rem]">{movie.title}</h1>
+        <p>{movie.duration}</p>
+        <p className="text-teal-700">{movie.genre}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,30 +1,23 @@
+"use client"
+import { addOrRemoveFavourite } from "@/lib/actions/user.actions";
 import { movieInterface } from "@/lib/database/models/movie.model";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
-import { FaCirclePlay } from "react-icons/fa6";
+
+import MovieCard from "./MovieCard";
 
 const Trending = ({ movies }: { movies: movieInterface[] }) => {
+  const {data: session} = useSession()
+
+
+
   return (
     <div className="text-white relative w-[95%] m-auto mt-5">
       <h1 className=" text-[1.5rem]">Trending Now</h1>
       <div className="h-auto flex flex-wrap mt-2">
         {movies.map((movie) => (
-          <div className="m-2 rounded-sm cursor-pointer hover:scale-110 hover:translate-y-[-2vw] trending">
-            <Image
-              src={movie?.thumbnailUrl}
-              alt="thumbnail"
-              width={0}
-              height={0}
-              className="h-[8rem] w-[16rem] rounded-sm"
-              unoptimized
-            />
-            <div className="w-full h-full opacity-0 trendingDesc mt-5 text-[0.8rem] pl-2">
-              <FaCirclePlay fontSize={30} className="mb-2 inline-block"/>
-              <h1 className="inline-block ml-2 text-[1.2rem]">{movie.title}</h1>
-              <p>{movie.duration}</p>
-              <p className="text-teal-700">{movie.genre}</p>
-            </div>
-          </div>
+          <MovieCard movie={movie}/>
         ))}
       </div>
     </div>
