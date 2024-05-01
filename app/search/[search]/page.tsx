8d87db497e8d484db3f9e423b434b5cba240fe96
Devansh Khare <devanshkhare0@gmail.com>
2024-05-01 13:41:44 +0530

    temp changes

---
+++ app/search/[search]/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const page = ({params}) => {
+    console.log(params)
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default page
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@
 import { NavItems, DropdownMenuItems } from '@/Constants'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import { CiSearch } from "react-icons/ci";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import {
@@ -15,8 +15,21 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { IoIosLogOut } from "react-icons/io";
 import { signOut } from 'next-auth/react';
-
+import { Input } from "@/components/ui/input"
+import { useRouter } from 'next/navigation'
 const Navbar = () => {
+  const router = useRouter()
+const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+  let timer;
+
+  const debouncedSearch = () => {
+    router.push(`/search/${event.target.value}`);
+    console.log(event?.target?.value);
+  };
+
+  clearTimeout(timer); // Clear any previous timer
+  timer = setTimeout(debouncedSearch, 1000);
+};
   return (
     <>
       <div className="mx-[2rem] py-4 px-2 flex flex-row items-center sticky z-10">
@@ -37,29 +50,40 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        <div className="text-white ml-[auto] gap-6 flex flex-row mr-[1rem]">
-          <CiSearch fontSize={25} />
-          <IoIosNotificationsOutline fontSize={25} />
+        <div className="text-white ml-[auto] gap-6 flex flex-row mr-[1rem] items-center">
+
+          
+           <Input className='bg-transparent border-gray-500 focus-visible:ring-offset-0'  onChange={handleSearch} />
+          <CiSearch fontSize={50}className='cursor-pointer ml-[-1rem]'/>
+
+          <IoIosNotificationsOutline fontSize={50}/>
           <DropdownMenu>
             <DropdownMenuTrigger>
               <Image
                 src="/images/avatar.png"
                 alt="avatar"
-                width={30}
-                height={30}
+                width={50}
+                height={50}
                 className="outline-none border-none"
               />
             </DropdownMenuTrigger>
-            <DropdownMenuContent className='bg-[#0f1014] border text-white'>
+            <DropdownMenuContent className="bg-[#0f1014] border text-white">
               {DropdownMenuItems.map((item) => (
-                <DropdownMenuItem className="cursor-pointer hover:bg-slate-800 text-gray-400" key={item.text}>
-                    <Link href={item.link}>
-                        {item.text}
-                    </Link>
+                <DropdownMenuItem
+                  className="cursor-pointer hover:bg-slate-800 text-gray-400"
+                  key={item.text}
+                >
+                  <Link href={item.link}>{item.text}</Link>
                 </DropdownMenuItem>
               ))}
-              <DropdownMenuSeparator/>
-              <DropdownMenuItem className='cursor-pointer' onClick={()=>signOut()}><IoIosLogOut fontSize={23}/>&nbsp;Logout</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onClick={() => signOut()}
+              >
+                <IoIosLogOut fontSize={23} />
+                &nbsp;Logout
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
