14da9f958e602b19600d744079eb385bfc019db6
Devansh Khare <devanshkhare0@gmail.com>
2024-05-01 18:16:34 +0530

    fixed homepage bug

--- app/(signed)/profiles/page.tsx
+++ app/(signed)/profiles/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import SessionContext from '@/context/SessionContext'
 import Image from 'next/image'
-import Link from 'next/link'
 import React from 'react'
 import { useContext } from 'react'
 import { useRouter } from 'next/navigation'
@@ -9,7 +8,7 @@ import { useRouter } from 'next/navigation'
 const page = () => {
   const sessionString = useContext(SessionContext)
   const router = useRouter()
-  let session
+  let session:any
   if (sessionString){
     session = JSON.parse(sessionString)
   }
@@ -18,7 +17,7 @@ const page = () => {
         <div className='flex w-[80%] h-[70%] items-center flex-col justify-center'>
             <div className='text-white self-start text-[2rem] font-bold text-center w-full'>Who is watching?</div>
             <div className='flex items-center justify-center w-[80%] h-[80%]'>
-                <div className='flex items-center flex-col cursor-pointer' onClick={()=>router.replace("/")}>
+                <div className='flex items-center flex-col cursor-pointer' onClick={()=>router.replace(`/?logged=true`)}>
                     <Image src="/images/avatar.png" alt='avatar' height={0} width={0} className='rounded-sm h-[8rem] w-[8rem]' unoptimized/>
                     <p className='text-white mt-[2rem]'>{session?.user?.username}</p>
                 </div>
