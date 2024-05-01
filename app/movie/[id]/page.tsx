3db67beda66cb820b3e9fc451f8dae8949a772dc
Devansh Khare <devanshkhare0@gmail.com>
2024-05-01 18:52:00 +0530

    fixed redirection issue

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -8,7 +8,7 @@ const page = async ({ params }: { params: { id: string } }) => {
   const movie = await getMovieById(movieId);
   return (
     <div className="text-white">
-      <Link href={`/profiles`} className="relative z-[999]">
+      <Link href={`/`} className="relative z-[999]">
         <div className="flex flex-row items-center mt-10 ml-10 gap-3 cursor-pointer">
           <FaChevronLeft />
           <h1 >Watching {movie?.title}</h1>
