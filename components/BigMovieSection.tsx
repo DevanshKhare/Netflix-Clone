03858a4d98746b6fac31e6ea7c0c1bf23c571fd6
Devansh Khare <devanshkhare0@gmail.com>
2024-05-03 00:16:25 +0530

    fixed big movie section responsiveness

--- components/BigMovieSection.tsx
+++ components/BigMovieSection.tsx
@@ -8,7 +8,7 @@ const BigMovieSection = ({ movies }: { movies: movieInterface[] }) => {
   if (!movies?.length) return <div>Loading...</div>;
   const randomMovie = movies[Math.floor(Math.random() * movies?.length)];
   return (
-    <div className="relative w-full h-[56.5vh] top-0 text-white">
+    <div className="relative w-full h-[56.5vh] max-sm:h-[30vh] top-0 text-white">
       <video
         src={randomMovie?.videoUrl}
         muted
@@ -16,9 +16,9 @@ const BigMovieSection = ({ movies }: { movies: movieInterface[] }) => {
         className="relative h-[100%] w-screen object-cover brightness-[60%]"
         loop
       ></video>
-      <div className="absolute top-[30%] left-[5rem] max-w-[40%]">
-        <h1 className="text-[2rem] mb-[2rem]">{randomMovie?.title}</h1>
-        <p className="text-slate-300">{randomMovie?.description}</p>
+      <div className="absolute top-[30%] left-[5rem] max-w-[40%] max-sm:top-[20%] max-sm:left-[2rem] max-sm:max-w-[80%]">
+        <h1 className="text-[2rem] mb-[0.5rem] lg:mb-[2rem] max-sm:max-w[60%]">{randomMovie?.title}</h1>
+        <p className="text-slate-300 truncate">{randomMovie?.description}</p>
         <div className="flex gap-5 items-center mt-5">
           <button className="text-white p-2 rounded-md bg-white bg-opacity-30 hover:bg-opacity-20 flex items-center">
           <IoIosInformationCircleOutline className="mr-1" fontSize={20} />
